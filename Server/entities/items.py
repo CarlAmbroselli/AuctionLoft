6c715eebeb4a355bf68feb68bea13dfd519d5f51
Carl Ambroselli <git@carl-ambroselli.de>
2014-04-20 21:48:11 +0200

    Added Endpoints /items and /item/{id}

---
+++ Server/entities/items.py
@@ -0,0 +1,15 @@
+from protorpc import messages
+
+class Item(messages.Message):
+    """Item in the shop"""
+    title       = messages.StringField(1)
+    description = messages.StringField(2)
+    expiration  = messages.StringField(3)
+    price       = messages.StringField(4)
+    item_id     = messages.StringField(5, required=True)
+    owner       = messages.StringField(6)
+
+
+class ItemCollection(messages.Message):
+    """Collection of Items."""
+    items = messages.MessageField(Item, 1, repeated=True)
